b3822b2d280714087c68398fe887bf9aa124cbae
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2022-11-07 08:57:09 -0300

    Telas prontas

--- controller/admin/adminController.js
+++ controller/admin/adminController.js
@@ -1,7 +1,11 @@
 //função que lista todos ítens
-async function lst(req, res) {}
+async function lst(req, res) {
+  res.render("admin/admin/lst");
+}
 //função que lista todos ítens de acordo com pesquisa
-async function filtro(req, res) {}
+async function filtro(req, res) {
+  res.render("admin/admin/lst");
+}
 //função que abre a tela de add
 async function abreadd(req, res) {
   res.render("admin/admin/add");
@@ -9,7 +13,9 @@ async function abreadd(req, res) {
 //função que adiciona
 async function add(req, res) {}
 //função que abre tela de edt
-async function abreedt(req, res) {}
+async function abreedt(req, res) {
+  res.render("admin/admin/edt");
+}
 //função que edita
 async function edt(req, res) {}
 //função que deleta ítens
